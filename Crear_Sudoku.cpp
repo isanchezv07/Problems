fd57568949c14965e66f03bd58b35fc12974dc34
¯\_(ツ)_/¯ <95550861+isanchezv07@users.noreply.github.com>
2023-06-14 09:16:10 -0600

    Update Crear_Sudoku.cpp

--- Crear_Sudoku.cpp
+++ Crear_Sudoku.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-const int N = 9; // Tamaño del sudoku
+const int N = 9; // Size of the sudoku
 
-// Función para imprimir el sudoku
+// Function to print the sudoku
 void printSudoku(int sudoku[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -17,21 +17,21 @@ void printSudoku(int sudoku[N][N]) {
     }
 }
 
-// Función para comprobar si un número se puede colocar en una posición determinada del sudoku
+// Function to check if a number can be placed at a particular position in the sudoku
 bool isValid(int sudoku[N][N], int row, int col, int num) {
-    // Comprobar si el número está en la fila
+    // Check if the number exists in the row
     for (int i = 0; i < N; i++) {
         if (sudoku[row][i] == num) {
             return false;
         }
     }
-    // Comprobar si el número está en la columna
+    // Check if the number exists in the column
     for (int i = 0; i < N; i++) {
         if (sudoku[i][col] == num) {
             return false;
         }
     }
-    // Comprobar si el número está en el cuadrante
+    // Check if the number exists in the 3x3 quadrant
     int rowStart = (row / 3) * 3;
     int colStart = (col / 3) * 3;
     for (int i = rowStart; i < rowStart + 3; i++) {
@@ -44,12 +44,12 @@ bool isValid(int sudoku[N][N], int row, int col, int num) {
     return true;
 }
 
-// Función para resolver el sudoku mediante backtracking
+// Function to solve the sudoku using backtracking
 bool solveSudoku(int sudoku[N][N]) {
     int row = -1;
     int col = -1;
     bool isEmpty = true;
-    // Buscar una posición vacía
+    // Find an empty position
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (sudoku[i][j] == 0) {
@@ -63,11 +63,11 @@ bool solveSudoku(int sudoku[N][N]) {
             break;
         }
     }
-    // Si no hay más posiciones vacías, el sudoku está resuelto
+    // If there are no more empty positions, the sudoku is solved
     if (isEmpty) {
         return true;
     }
-    // Probar con números del 1 al 9
+    // Try numbers from 1 to 9
     for (int num = 1; num <= 9; num++) {
         if (isValid(sudoku, row, col, num)) {
             sudoku[row][col] = num;
@@ -80,19 +80,19 @@ bool solveSudoku(int sudoku[N][N]) {
     return false;
 }
 
-// Función para generar un sudoku aleatorio
+// Function to generate a random sudoku
 void generateSudoku(int sudoku[N][N]) {
-    // Inicializar el sudoku a cero
+    // Initialize the sudoku to zero
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             sudoku[i][j] = 0;
         }
     }
-    // Resolver el sudoku vacío
+    // Solve the empty sudoku
     solveSudoku(sudoku);
-    // Quitar números aleatorios para crear el sudoku con huecos
+    // Remove random numbers to create the sudoku with holes
     srand(time(NULL));
-    int numHoles = rand() % 40 + 20; // Número aleatorio de huecos entre 20 y 59
+    int numHoles = rand() % 40 + 20; // Random number of holes between 20 and 59
     for (int i = 0; i < numHoles; i++) {
         int row = rand() % 9;
         int col = rand() % 9;
@@ -111,4 +111,4 @@ int main() {
     return 0;
 }
 
-//Isanchezv
+// Isanchezv
