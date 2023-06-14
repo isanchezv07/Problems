d2159ad791ac5fa7a12370b97ab2d4525abce99b
¯\_(ツ)_/¯ <95550861+isanchezv07@users.noreply.github.com>
2023-06-14 09:17:38 -0600

    Update Resolver_Sudoku.cpp

--- Resolver_Sudoku.cpp
+++ Resolver_Sudoku.cpp
@@ -5,106 +5,104 @@ using namespace std;
 int P[9][9];
 bool flag = false;
 
-// Entrada de proceso 
-void input(){
-	char t;
-	cout << "Introduzca un tablero de ajedrez, vacío con 0 para no hay valor \n";
-	for(int i = 0; i < 9; i ++){
-		for(int j = 0; j < 9; j ++){
-			cin >> t;
-			P[i][j] = t - '0';
-		}
-	}
+// Process input
+void input() {
+    char t;
+    cout << "Enter a chessboard, empty with 0 for no value:\n";
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> t;
+            P[i][j] = t - '0';
+        }
+    }
 }
 
-// Salida del proceso 
-void output(){
-	for(int i = 0; i < 9; i ++){
-		for(int j = 0; j < 9; j ++){
-			
-			if(P[i][j]){
-				cout << P[i][j];	
-			}else{
-				cout << " ";
-			}
-			
-			if(j%3 == 2) cout << "  ";
-		}
-		if(i%3 == 2) cout << endl;
-		cout << endl;
-	}
-	
+// Process output
+void output() {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (P[i][j]) {
+                cout << P[i][j];
+            } else {
+                cout << " ";
+            }
+
+            if (j % 3 == 2)
+                cout << "  ";
+        }
+        if (i % 3 == 2)
+            cout << endl;
+        cout << endl;
+    }
 }
 
-// Determinar si se colocará cumple con las condiciones 
-bool check(int n, int num){
-	
-	// Fila  
-	for(int i = 0; i < 9; i ++){
-		if(P[n/9][i] == num){
-			return false;
-		}
-	} 
-	
-	// columna 
-	for(int i = 0; i < 9; i ++){
-		if(P[i][n%9] == num){
-			return false;
-		}
-	} 
-	
-	// Palacio
+// Check if placing a number at a specific position satisfies the conditions
+bool check(int n, int num) {
+    // Row
+    for (int i = 0; i < 9; i++) {
+        if (P[n / 9][i] == num) {
+            return false;
+        }
+    }
+
+    // Column
+    for (int i = 0; i < 9; i++) {
+        if (P[i][n % 9] == num) {
+            return false;
+        }
+    }
+
+    // Palace
     int x = n / 9 / 3 * 3;
     int y = n % 9 / 3 * 3;
-    for(int i = 0; i < 3; i ++){
-    	for(int j = 0; j < 3; j ++){
-    		if(P[x+i][y+j] == num){
-    			return false;
-    		}
-    	}
-    } 
-    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (P[x + i][y + j] == num) {
+                return false;
+            }
+        }
+    }
+
     return true;
 }
 
 // [n/9][n%9]
-void dfs(int n){
-	
-	// éxito 
-	if(n > 80){
-		flag = true;
-		return;
-	}
-	
-	int x = n/9, y = n%9;
-	// salto de valor 
-	if(P[x][y]){
-		dfs(n+1);
-		return;
-	}
-	// Atravesar 
-	for(int i = 1; i <= 9; i ++){
-		// juez 
-		if(check(n, i)){
-			// Asignación de éxito del juicio 
-			P[x][y] = i;
-			dfs(n+1);
-			// Juzgar si está completo al salir, salir cuando esté completo 
-			if(flag){
-				return;
-			}
-			// no completado reinicia el tablero 
-			P[x][y] = 0;
-		}
-	}
+void dfs(int n) {
+    // Success
+    if (n > 80) {
+        flag = true;
+        return;
+    }
+
+    int x = n / 9, y = n % 9;
+    // Skip filled values
+    if (P[x][y]) {
+        dfs(n + 1);
+        return;
+    }
+    // Traverse
+    for (int i = 1; i <= 9; i++) {
+        // Check if valid
+        if (check(n, i)) {
+            // Assign and recursively call
+            P[x][y] = i;
+            dfs(n + 1);
+            // Check if complete on exit, terminate if complete
+            if (flag) {
+                return;
+            }
+            // Not complete, reset the position
+            P[x][y] = 0;
+        }
+    }
 }
 
-int main(){
-	input();
-	cout << endl << endl; 
-	dfs(0);
-	output();
-	return 0;
-} 
+int main() {
+    input();
+    cout << endl << endl;
+    dfs(0);
+    output();
+    return 0;
+}
 
-//Isanchezv 
+// Isanchezv
